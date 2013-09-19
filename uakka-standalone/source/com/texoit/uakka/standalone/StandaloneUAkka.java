9cb84139d221214b7b99542eae5c984e391f292e
Miere Teixeira <miere00@gmail.com>
2013-09-19 10:00:05 -0300

    Exposing standalone better life cycle methods

--- uakka-standalone/source/com/texoit/uakka/standalone/StandaloneUAkka.java
+++ uakka-standalone/source/com/texoit/uakka/standalone/StandaloneUAkka.java
@@ -3,6 +3,7 @@ package com.texoit.uakka.standalone;
 import java.io.IOException;
 import java.util.Set;
 
+import com.texoit.uakka.api.Injectables;
 import com.texoit.uakka.api.exception.InjectionException;
 import com.texoit.uakka.service.AkkaInitialization;
 
@@ -11,25 +12,38 @@ import lombok.extern.java.Log;
 
 @Log
 @RequiredArgsConstructor
-public class StandaloneAkkaInitialization {
+public class StandaloneUAkka {
 
 	final String applicationName;
+	AkkaInitialization initialization;
+	StandaloneAkkaConfiguration configuration;
 
 	public void initialize() throws InjectionException, ClassNotFoundException, IOException {
 		log.info( "Initializing micro Akka..." );
 		Set<Class<?>> classes = retrieveAvailableClasses();
-		StandaloneAkkaConfiguration configuration = new StandaloneAkkaConfiguration( this.applicationName );
-		AkkaInitialization initialization = new AkkaInitialization( classes, configuration );
+		configuration = new StandaloneAkkaConfiguration( this.applicationName );
+		initialization = new AkkaInitialization( classes, configuration );
 		initialization.initialize();
-		configuration.getAkkaActors().getActorSystem().awaitTermination();
 	}
 
 	Set<Class<?>> retrieveAvailableClasses() throws IOException, ClassNotFoundException {
-		log.info( "Taking a look at classpath for actors..." );
+		log.info( "Searching for actors at classpath..." );
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		SystemResourceLoader resourceLoader = new SystemResourceLoader(
 				classLoader, SystemResourceLoader.DEFAULT_RESOURCE_PATH );
 		return resourceLoader.retrieveAvailableClasses();
+	}
+
+	public void awaitTermination() {
+		configuration.getAkkaActors().getActorSystem().awaitTermination();
+	}
+
+	public void shutdown() {
+		configuration.getAkkaActors().shutdown();
+	}
+	
+	public Injectables getInjectables(){
+		return initialization.getInjectables();
 	}
 
 	public static void main( String[] args ) throws InjectionException, ClassNotFoundException, IOException {
@@ -37,7 +51,9 @@ public class StandaloneAkkaInitialization {
 			System.out.println("Missing application name parameter.");
 			return;
 		}
-		new StandaloneAkkaInitialization( args[0] ).initialize();
-	}
 
+		StandaloneUAkka standalone = new StandaloneUAkka( args[0] );
+		standalone.initialize();
+		standalone.awaitTermination();
+	}
 }
