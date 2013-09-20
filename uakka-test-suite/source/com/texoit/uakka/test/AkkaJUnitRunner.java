bb5e12c947d5a9e3b79eaaa86a079683757f7cb8
Miere Teixeira <miere00@gmail.com>
2013-09-20 14:13:05 -0300

    AkkaJUnitRunner clean coded

--- uakka-test-suite/source/com/texoit/uakka/test/AkkaJUnitRunner.java
+++ uakka-test-suite/source/com/texoit/uakka/test/AkkaJUnitRunner.java
@@ -6,15 +6,11 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Executors;
 
-import lombok.AllArgsConstructor;
-
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
-import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
-import com.texoit.uakka.inject.HandledInjectableClass;
 import com.texoit.uakka.service.factories.ReferenceActorClassFactory;
 import com.texoit.uakka.service.factories.ServiceActorClassFactory;
 import com.texoit.uakka.standalone.StandaloneUAkka;
@@ -67,40 +63,5 @@ public class AkkaJUnitRunner extends Runner {
 		AsynchronousClassRunner asynchronousClassRunner = new AsynchronousClassRunner(classRunner, notifier, standalone);
 		Executors.newSingleThreadExecutor().submit(asynchronousClassRunner);
 		standalone.awaitTermination();
-	}
-
-	public static class InjectableBlockJUnit4ClassRunner extends BlockJUnit4ClassRunner {
-		StandaloneUAkka standalone;
-		Class<?> targetClass;
-
-		public InjectableBlockJUnit4ClassRunner(StandaloneUAkka standalone, Class<?> klass)
-				throws InitializationError {
-			super(klass);
-			this.standalone = standalone;
-			this.targetClass = klass;
-		}
-
-		@Override
-		protected Object createTest() throws Exception {
-			HandledInjectableClass injectableClass = new HandledInjectableClass(
-					standalone.getInjectables(), targetClass);
-			Object newInstance = injectableClass.newInstance();
-			System.out.println("created test: " + newInstance);
-			return newInstance;
-		}
-	}
-
-	@AllArgsConstructor
-	public static class AsynchronousClassRunner implements Runnable {
-
-		BlockJUnit4ClassRunner classRunner;
-		RunNotifier notifier;
-		StandaloneUAkka standalone;
-
-		@Override
-		public void run() {
-			classRunner.run(notifier);
-			standalone.shutdown();
-		}
 	}
 }
