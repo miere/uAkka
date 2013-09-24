41a9c5927bef5f991c13acb77a36426ff4a0f438
Miere Teixeira <miere00@gmail.com>
2013-09-24 18:25:42 -0300

    Added ActorSystem class factory

---
+++ uakka-service/source/com/texoit/uakka/service/DefaultActorSystemClassFactory.java
@@ -0,0 +1,19 @@
+package com.texoit.uakka.service;
+
+import java.lang.reflect.Field;
+
+import akka.actor.ActorSystem;
+
+import com.texoit.uakka.api.exception.InjectionException;
+import com.texoit.uakka.inject.AbstractInjectableClassFactory;
+
+public class DefaultActorSystemClassFactory 
+	extends AbstractInjectableClassFactory<ActorSystem> {
+
+	@Override
+	public Object newInstance(Object instance, Field injectableField)
+			throws InjectionException {
+		return getConfiguration().getAkkaActors().getActorSystem();
+	}
+
+}
